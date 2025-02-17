9c23642a894146d31365e731f563cbd5ddb76478
AndreSosa21 <andreasosro@unisabana.edu.co>
2025-02-16 22:04:32 -0500

    cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const transactionRoutes = require("./routes/transactionRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://state-pattern-andresosa21-andresosa21s-projects.vercel.app"],
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
 
 // Ruta de verificación para saber si el servidor está en línea
 app.get("/", (req, res) => {
